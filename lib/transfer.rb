a82fa0ec62d0d6d70b5556aacd03c676dd2eeb0e
jomariepolanco <jomariepolanco@gmail.com>
2020-10-09 09:42:39 -0400

    bank_account done

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,24 @@
 class Transfer
   # your code here
+  attr_reader :sender, :receiver, :status, :amount 
+  attr_writer :sender, :receiver, :status, :amoutnt
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver 
+    @status = "pending"
+    @amount = amount 
+  end
+
+  def valid?
+
+  end
+
+  def execute_transaction
+
+  end
+
+  def reverse_transfer
+
+  end
 end
